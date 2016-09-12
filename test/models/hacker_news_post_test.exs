d864dd4fc2f45e07cc84c5a50cd3e1f60e983b46
stephen <huangjun604@gmail.com>
2016-09-12 10:48:33 +1000

    Add test for post model

--- test/models/hacker_news_post_test.exs
+++ test/models/hacker_news_post_test.exs
@@ -3,8 +3,9 @@ defmodule HackerNews.HackerNewsPostTest do
 
   alias HackerNews.HackerNewsPost
 
-  @valid_attrs %{author: "some content", story_title: "some content", story_url: "some content"}
+  @valid_attrs %{author: "Stephen", story_title: "some content", story_url: "some content", object_id: 1842543}
   @invalid_attrs %{}
+  @valid_json %{"author" => "Stephen", "story_title" => "Elixir Australia", "story_url" => "http://hello", "objectID" => "1842543"}
 
   test "changeset with valid attributes" do
     changeset = HackerNewsPost.changeset(%HackerNewsPost{}, @valid_attrs)
@@ -14,5 +15,29 @@ defmodule HackerNews.HackerNewsPostTest do
   test "changeset with invalid attributes" do
     changeset = HackerNewsPost.changeset(%HackerNewsPost{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "parse/1 parses the post successfully when post data given" do
+    post = HackerNewsPost.parse @valid_json
+
+    assert post.author == "Stephen"
+    assert post.story_title == "Elixir Australia"
+    assert post.story_url == "http://hello"
+    assert post.object_id == 1842543
+  end
+
+  test "insert/1 inserts the post successfully when the post does not exist" do
+    post = HackerNewsPost.parse @valid_json
+    post_db = HackerNewsPost.insert post
+
+    assert post_db.author == post.author
+  end
+
+  test "insert/1 not insert the post when the post exists" do
+    post = HackerNewsPost.parse @valid_json
+    HackerNewsPost.insert post
+    post_db = HackerNewsPost.insert post
+    
+    assert post_db == nil
   end
 end
